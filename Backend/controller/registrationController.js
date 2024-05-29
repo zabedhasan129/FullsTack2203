1119b5096c4bc4c220816d47a7adf9df609e4e49
zabedhasan129 <143251101+zabedhasan129@users.noreply.github.com>
2024-05-29 22:57:34 +0600

    check already exixt email

--- Backend/controller/registrationController.js
+++ Backend/controller/registrationController.js
@@ -2,7 +2,7 @@ const blankInput = require("../helpers/blankInput");
 const emailValidator = require("../helpers/emailvalidator");
 const Passwordvalidator = require("../helpers/passwordvalidator");
 let User = require("../model/userModel")
-let registrationController = (req, res) => {
+let registrationController = async (req, res) => {
     const { username, email, password } = req.body;
     console.log(username, email, password);
 
@@ -21,13 +21,32 @@ let registrationController = (req, res) => {
         res.send({ error: "At least 6 digit" })
     }
     else {
-        let user = new User({
-            username: username,
-            email: email,
-            password: password,
-        })
-        user.save();
-        res.send(user);
+        let existingEmail = await User.find({ email: email });
+
+        console.log(existingEmail)
+
+        if(existingEmail.length > 0){
+            res.send({error:`${email} Already exixst`})
+        }
+        else{
+            let user = new User({
+                username: username,
+                email: email,
+                password: password,
+            })
+            user.save();
+            res.send(user);
+        }
+
+
+
+
+
+
+
+
+
+        
     }
 
 }
